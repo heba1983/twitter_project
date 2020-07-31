23076e6fc83a649b351cb34e3b3dd9925e7ef7ea
heba <kindharts@hotmail.com>
2020-07-31 22:55:25 +0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -6,11 +6,12 @@ var tweets = [];
 var count;
 
 function createTweet(e) {
+    e.preventDefault();
     var tweetobj = {
         author: document.getElementById("usname").value,
         tweet: document.getElementById("tweetbox").value
     }
-     e.preventDefault();
+     
     tweets.push(tweetobj);//for saving in array
     document.forms[0].reset();//to clear the form for the next entries
     //for display author & tweet
